1e61cae42b6c6de9d8aefc00ba28bbe961b22686
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2023-04-29 07:53:06 +0530

    Added roomId

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-// Node server which will handle socket io connections
-const express = require('express')
-app = express()
-http = require('http').createServer(app)
-const io = require('socket.io')(http)
-const PORT = process.env.PORT || 8000
-
-
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 8000;
 
 http.listen(PORT, function() {
-    console.log("Server started on PORT: 8000")
-})
+    console.log("Server started on PORT: 8000");
+});
 
 const users = {};
 
 io.on('connection', socket =>{
 
     // If any new user joins, let other users connected to the server know!
-    socket.on('new-user-joined', (name, userId, peerId) =>{ 
+    socket.on('new-user-joined', (name, userId, roomId, peerId) =>{ 
         users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name, userId, peerId);
+        socket.join(roomId);
+        socket.broadcast.to(roomId).emit('user-joined', name, userId, peerId);
     });
 
     // If someone sends a message, broadcast it to other people
-    socket.on('send', (message, peerId) =>{
-        socket.broadcast.emit('receive', {message: message, name: users[socket.id], userId: socket.id, peerId: peerId})
+    socket.on('send', (message, roomId, peerId) =>{
+        socket.broadcast.to(roomId).emit('receive', {message: message, name: users[socket.id], userId: socket.id, peerId: peerId})
         console.log(socket.id, "sent msg")
     });
 
     // If someone leaves the chat, let others know 
-    socket.on('disconnect', message =>{
-        socket.broadcast.emit('left', users[socket.id]);
+    socket.on('disconnect', (roomId) =>{
+        socket.broadcast.to(roomId).emit('left', users[socket.id]);
         delete users[socket.id];
     });
 
@@ -61,4 +59,9 @@ io.on('connection', socket =>{
         })
     })
 
-})
+    // Get the list of connected users in a room
+    socket.on('get-users', () => {
+        socket.emit('users-in-room', users);
+    });
+
+});
